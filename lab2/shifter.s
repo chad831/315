f3c7abc94ff16927c5d66f1317826934401779c5
Nghia Nguyen Van <fatboy_dn_2193@yahoo.com>
2016-04-08 12:47:45 -0700

    Update bintohex to the shifter

--- lab2/shifter.s
+++ lab2/shifter.s
@@ -17,6 +17,8 @@
    shiftMask2: .word 0x00080000  # used for bit 19
    msg: .asciiz   "Sorry, format of input is not valid."
    format: .asciiz "\n"
+	buff: .space 9 # storage for 9 bytes, less spaces
+   table: .byte 0x30, 0x31, 0x32, 0x33, 0x34, 0x35 0x36, 0x37, 0x38, 0x39, 0x41, 0x42, 0x43, 0x44, 0x45, 0x46  # table of character values
 
 .text
 
@@ -25,9 +27,19 @@ main:
 
    lw $a0, val1    # load first input value
    jal shifter
+	
+	la $t1 table # load address of table
+   la $a1 buff  # load address of buffer
    move $a0, $v0  # load result to print
-   li $v0, 1 
-   syscall
+	
+   # call bintohex func
+   jal bintohex
+	
+	# a1 will be at the last index in buffer when ret
+	sb $zero 0($a1)
+   la $a0 buff
+	li $v0 4
+	syscall
 
    li $v0, 4
    la $a0, format
@@ -35,11 +47,20 @@ main:
 
    lw $a0, val2   # load second input value
    jal shifter
+   
+	la $t1 table # load address of table
+   la $a1 buff  # load address of buffer
    move $a0, $v0  # load result to print
-   li $v0, 1 
-   syscall
+	
+   # call bintohex func
+   jal bintohex
 
-
+   # a1 will be at the last index in buffer when ret
+	sb $zero 0($a1)
+   la $a0 buff
+	li $v0 4
+	syscall
+	
    li $v0 10
    syscall
 
@@ -125,4 +146,43 @@ shifter:
    addi $sp, $sp, 4    
    jr $ra
 
+##############################################
+# function bintohex
+# function makes the following assumptions: 
+# a0 => value
+# a1 => buffer location
+# t1 => table
+# t5 => loop counter
+# ther is no return value
+
+bintohex:
+   addi $sp $sp -4 # build stack, ra, fp, vars, etc
+   sw $ra 0($sp)
+   addi $sp $sp -4    
+   sw $fp 0($sp)
+   move $fp $sp
+   addi $sp $sp -4 # save temp register $t0
+	sw $t0 0($sp)
+	
+	
+   li $t5 8 			# set counter to 8
+loop:
+   srl $t0 $a0 28 	# get the first 4 bits of a0 into t0
+	sll $a0 $a0 4 	 	# shift left 4 bits to delete the first 4 bits we got above
+	add $t0 $t0 $t1 	# t0 will not be at the right index at the table
+	
+	lb  $t0 0($t0)		# the hex value (character) is now save in t0
+	sb  $t0 0($a1)		# save the hex value (character) into the buff
+	addi $a1 $a1 1		# increase to the next location in the buff
+	addi $t5, $t5, -1 # decrease the counter by 1
+	bne $t5 $zero loop	# run until the counter reach 0 (run 8 times)
+	
+   lw $t0 0($sp)		# restore the temp t0
+	addi $sp $sp 4
+	lw $fp 0($sp)    	# restore frame
+	addi $sp $sp 4
+	lw $ra 0($sp)		# retore the ret address
+	addi $sp $sp 4    # stack is now has nothing
+	jr $ra 				
+   
 .end
